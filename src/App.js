c55ba392cdc932b74bbd1f3adb56930194039977
Jukka Aho <ahojukka5@gmail.com>
2021-01-10 19:03:53 +0700

    Add link to GitHub authorization service

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from 'react';
+
+const CLIENT_ID = '9137d43268b8e5dfdd48';
+const REDIRECT_URI = 'http://localhost:3000';
+const LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`;
+
 function App() {
-  return <div>Login page</div>;
+  return (
+    <div>
+      <a href={LOGIN_URL}>Login using GitHub</a>
+    </div>
+  );
 }
 
 export default App;
